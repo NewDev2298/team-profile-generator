7d7cf20a41fe1c8c7bcb5873897367e6a08cc84f
Terrance Smith <terrancesmith@Laquishas-MacBook-Air.local>
2023-02-06 09:39:02 -0500

    continued adding variables for employee test and engineer test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,15 +3,19 @@ const Employee = require("../lib/Employee");
 describe("Employee", () => {
 
   it('should have a name', () => {
-    
+    const employee = new Employee('Royale', 1, 'royale@email.com');
+    expect(employee.name).toEqual('Royale');
   })
   it('should have an id', () => {
-
+    const employee = new Employee('Royale', 1, 'royale@email.com');
+    expect(employee.id).toEqual(1);
   })
   it('should have an email', () => {
-
+    const employee = new Employee('Royale', 1, 'royale@email.com');
+    expect(employee.email).toEqual('royale@email.com');
   })
   it('should have a role', () => {
-
+    const employee = new Employee('Royale', 1, 'royale@email.com');
+    expect(employee.getRole).toEqual('Employee');
   })
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,6 +3,7 @@ const Engineer = require("../lib/Engineer");
 describe("Engineer", () => {
 
   it('should have a GitHub', () => {
-
+    const engineer = new Engineer('Royale', 1, 'royale@email.com', 'kiDev' );
+    expect(engineer.gitHub).toEqual('kiDev');
   })
 });
