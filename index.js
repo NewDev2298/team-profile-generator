7acdfe86829de987f20b26e5415a7fed890d5258
Terrance Smith <terrancesmith@Laquishas-MacBook-Air.local>
2023-02-07 13:58:09 -0500

    added defaults to prompt ?s

--- index.js
+++ index.js
@@ -13,64 +13,81 @@ const managerQuestions = [
     {
         message: 'Please add name of manager.',
         name: 'name',
+        default: 'Terrance'
     },
     {
         message:'What is the manager\'s id number?' ,
         name: 'id',
+        default: 4842
     },
     {
         message: 'What is the manager\'s email address?',
         name: 'email',
+        default: 'terrance@student.com'
     },
     {
         message: 'What is the manager\'s office number?',
         name: 'officeNumber',
+        default: 352
     },
-    ];
+];
 
 
 const engineerQuestions = [
     {
         message: 'Please add name of engineer.',
         name: 'name',
+        default: 'Quisha'
     },
     {
         message:'What is the engineer\'s id number?' ,
         name: 'id',
+        default: 1021
     },
     {
         message: 'What is the engineer\'s email address?',
         name: 'email',
+        default: 'la.mcf@student.com'
     },
     {
         message: 'What is the engineer\'s GitHub name?',
         name: 'gitHub',
+        default: 'lalli'
     },
-    ];
+];
     
 
 const internQuestions = [
     {
         message: 'Please add name of intern.',
         name: 'name',
+        default: 'Mel'
     },
     {
         message:'What is the intern\'s id number?' ,
         name: 'id',
+        default: 072
     },
     {
         message: 'What is the intern\'s email address?',
         name: 'email',
+        default: 'steelers@herewego.com'
     },
     {
         message: 'What is the intern\'s school?',
         name: 'school',
+        default: 'FCHS'
     },
-    ];
+];
 
 
 prompt (managerQuestions).then(({name, id, email, officeNumber}) => {
     const manager = new Manager(name, id, email, officeNumber);
     teamMem.push(manager);
-});
+})
+    .then(() => {
+        prompt({
+            message: 'Would you like '
+        })
+    })
 
