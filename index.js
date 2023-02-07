1d65e52eea771897eaf7beaacf1e8f95b6720e55
Terrance Smith <terrancesmith@Laquishas-MacBook-Air.local>
2023-02-07 13:10:57 -0500

    started adding prompts to index.js

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+// Instructor assisted with prompt code
+const prompt = inquirer.createPromptModule();
+const teamMem = [];
 
-
+prompt([
+    {
+        message: 'Please add name of manager.',
+        name: 'name',
+    },
+    {
+        message:'What is the manager\'s id number?' ,
+        name: 'id',
+    },
+    {
+        message: 'What is the manager\'s email address?',
+        name: 'email',
+    },
+    {
+        message: 'What is the manager\'s office number?',
+        name: 'officeNumber',
+    },
+]).then(({name, id, email, officeNumber}) => {
+    const manager = new Manager(name, id, email, officeNumber);
+    teamMem.push(manager);
+});
 
